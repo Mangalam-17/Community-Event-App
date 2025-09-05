623f1629571fc2d971549047d99d524b3839225a
Mangalam Mishra <mangalamab17@gmail.com>
2025-09-05 12:23:44 +0530

    feat: Add base url for GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { App } from "./App.jsx"; // App is imported as a named export
 import { BrowserRouter } from "react-router-dom";
 import { EventProvider } from "./context/EventContext.jsx";
 import "./styles.css";
 
+// This variable is necessary to handle the correct base path for GitHub Pages.
+const basename = new URL(window.location.href).pathname
+  .split("/")
+  .slice(0, -1)
+  .join("/");
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <EventProvider>
         <App />
       </EventProvider>
